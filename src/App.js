81c1cfba9593031d06a986b32f28d112deb69bab
holgermanzke <manzkeholger@gmail.com>
2020-06-26 20:08:27 +0200

    Add Theming

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import GlobalStyles from "./components/GlobalStyles";
+import { ThemeProvider } from "emotion-theming";
+import main from "./themes/main";
 
 function App() {
-  return <GlobalStyles />;
+  return (
+    <ThemeProvider theme={main}>
+      <GlobalStyles />
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/themes/main.js
@@ -0,0 +1,9 @@
+const main = {
+  colors: {
+    font: "#0E3847",
+    background: "#EFEFEF",
+    ctaButton: "#2DB4E2",
+  },
+};
+
+export default main;
